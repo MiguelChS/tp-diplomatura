9db55cab6e8602cfb4427667ddb1697a6118a59b
Miguelchauca <miguelchauca@Miguelchaucas-MacBook-Pro.local>
2019-12-06 18:10:27 -0300

    primera subida

---
+++ js/mostrarocultar.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    function showContent() {
+        element = document.getElementById("content");
+        check = document.getElementById("check");
+        if (check.checked) {
+            element.style.display='block';
+        }
+        else {
+            element.style.display='none';
+        }
+    }
+</script>
